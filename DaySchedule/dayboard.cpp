97895b43ef7b42f52677476fa42511698c4be535
cirt9 <bartekxzxz@gmail.com>
2017-05-16 20:28:56 +0200

    I have made early concept of DayBoard;

--- DaySchedule/dayboard.cpp
+++ DaySchedule/dayboard.cpp
@@ -1,61 +1,159 @@
 #include "dayboard.h"
 
-#include <QLabel>
 #include <QTimeEdit>
 #include <QLineEdit>
 #include <QCheckBox>
-#include <QSpacerItem>
-
-#include <QHBoxLayout>
-#include <QVBoxLayout>
+#include <QPushButton>
 
 DayBoard::DayBoard(QWidget * parent) : QGroupBox(parent)
 {
-    setMaximumSize(800, 900);
-    setStyleSheet("background: yellow; padding:10px;");
+    setStyleSheet("DayBoard{background: rgb(255,240,235); border-radius: 10px;}");
 
-    //topLayout
-    QHBoxLayout * topLayout = new QHBoxLayout();
+    dayBoardLayout = new QVBoxLayout();
+    dayBoardLayout->setSpacing(20);
+    dayBoardLayout->setContentsMargins(0, 0, 0, 0);
+    setLayout(dayBoardLayout);
+    numberOfLayoutsInMainLayout = 0;
 
-    QLabel * icon = new QLabel();
-    icon->setText("ICON");
-    icon->setAlignment(Qt::AlignHCenter);
-    icon->setStyleSheet("background: green");
+    makeDateAndProgressLayout();
+    makeBottomIconsLayout();
 
-    QLabel * date = new QLabel();
-    date->setText("MONTH DAY YEAR");
-    //date->setMaximumHeight(50);
-    date->setAlignment(Qt::AlignHCenter);
-    date->setStyleSheet("background: green");
+    addNewActivityLayout();
+}
 
-    QLabel * progress = new QLabel();
-    progress->setText("100%");
-    progress->setAlignment(Qt::AlignHCenter);
-    progress->setStyleSheet("background: green");
 
-    topLayout->addWidget(icon);
-    topLayout->addWidget(date);
-    topLayout->addWidget(progress);
+void DayBoard::makeDateAndProgressLayout()
+{
+    QHBoxLayout * dateProgressLayout = new QHBoxLayout();
+    dateProgressLayout->setSpacing(0);
 
-    //botLayout
-    QHBoxLayout * botLayout = new QHBoxLayout();
+    QLabel * date = new QLabel("13.05.2017, Saturday");
+    date->setMaximumHeight(80);
+    date->setAlignment(Qt::AlignCenter);
+    date->setStyleSheet("background: rgb(51,102,153); border-right: 2px dotted rgb(255,240,235); padding: 10px;"
+                        "color: rgb(255,240,235); font-size: 35px; border-top-left-radius: 3px; border-bottom-left-radius: 3px");
 
-    QTimeEdit * t = new QTimeEdit();
-    QLineEdit * l = new QLineEdit();
-    QCheckBox * c = new QCheckBox();
-    t->setStyleSheet("background: green");
-    l->setStyleSheet("background: green");
-    c->setStyleSheet("background: green");
+    QLabel * progress = new QLabel("100%");
+    progress->setMaximumHeight(80);
+    progress->setAlignment(Qt::AlignCenter);
+    progress->setMaximumWidth(120);
+    progress->setStyleSheet("background: rgb(51,102,153); border-left: 2px dotted rgb(255,240,235); padding: 10px;"
+                            "color: rgb(255,240,235); font-size: 35px; border-top-right-radius: 3px; border-bottom-right-radius: 3px");
 
-    botLayout->addWidget(t);
-    botLayout->addWidget(l);
-    botLayout->addWidget(c);
+    dateProgressLayout->addWidget(date);
+    dateProgressLayout->addWidget(progress);
 
-    //totalLayout
+    dayBoardLayout->addLayout(dateProgressLayout);
+    numberOfLayoutsInMainLayout++;
+}
 
-    QVBoxLayout * vLayout = new QVBoxLayout();
-    vLayout->addLayout(topLayout);
-    vLayout->addLayout(botLayout);
+void DayBoard::makeBottomIconsLayout()
+{
+    QHBoxLayout * iconsBarLayout = new QHBoxLayout();
+    QGroupBox * iconsBarContainer = new QGroupBox();
+    iconsBarContainer->setMaximumHeight(80);
+    iconsBarContainer->setStyleSheet("background: rgb(51,102,153); padding: 10px; border-radius: 3px;");
+    iconsBarContainer->setLayout(iconsBarLayout);
 
-    setLayout(vLayout);
+    QPushButton * alarmsIcon = new QPushButton();
+    alarmsIcon->setStyleSheet("background-image:url(:/placeholderimage.jpg); border: none");
+    alarmsIcon->setFixedSize(40, 40);
+
+    QPushButton * addNewIcon = new QPushButton();
+    addNewIcon->setStyleSheet("background-image:url(:/placeholderimage.jpg); border: none");
+    addNewIcon->setFixedSize(40, 40);
+
+    QPushButton * deleteAllIcon = new QPushButton();
+    deleteAllIcon->setStyleSheet("background-image:url(:/placeholderimage.jpg); border: none");
+    deleteAllIcon->setFixedSize(40, 40);
+
+    QPushButton * copyIcon = new QPushButton();
+    copyIcon->setStyleSheet("background-image:url(:/placeholderimage.jpg); border: none");
+    copyIcon->setFixedSize(40, 40);
+
+    iconsBarLayout->addWidget(alarmsIcon);
+    iconsBarLayout->addWidget(addNewIcon);
+    iconsBarLayout->addWidget(deleteAllIcon);
+    iconsBarLayout->addWidget(copyIcon);
+
+    dayBoardLayout->addWidget(iconsBarContainer);
+    dayBoardLayout->setAlignment(iconsBarContainer, Qt::AlignBottom);
+    numberOfLayoutsInMainLayout++;
+}
+
+void DayBoard::addNewActivityLayout()
+{
+    QGroupBox * activitiesContainer = new QGroupBox();
+    activitiesContainer->setStyleSheet("background: rgb(51,102,153); margin-left: 20px; margin-right: 20px; border-radius: 3px;");
+
+    QHBoxLayout * timeRangeLayout = new QHBoxLayout();
+    QHBoxLayout * iconsLayout = new QHBoxLayout();
+
+    QLabel * fromLabel = new QLabel();
+    fromLabel->setText(QString("From: "));
+    fromLabel->setStyleSheet("background: rgb(255,240,235); font-size: 20px; border-top-right-radius: 0px; border-bottom-right-radius: 0px; margin: 0px;");
+    fromLabel->setAlignment(Qt::AlignCenter);
+    fromLabel->setFixedHeight(50);
+
+    QTimeEdit * fromTime = new QTimeEdit();
+    fromTime->setStyleSheet("background: rgb(255,240,235); font-size: 20px; border-radius: 0px; margin: 0px;");
+    fromTime->setFixedHeight(50);
+
+    QLabel * toLabel = new QLabel();
+    toLabel->setText(QString("To: "));
+    toLabel->setStyleSheet("background: rgb(255,240,235); font-size: 20px; border-radius: 0px; margin: 0px;");
+    toLabel->setAlignment(Qt::AlignCenter);
+    toLabel->setFixedHeight(50);
+
+    QTimeEdit * toTime = new QTimeEdit();
+    toTime->setStyleSheet("background: rgb(255,240,235); font-size: 20px; border-top-left-radius: 0px; border-bottom-left-radius: 0px; margin: 0px;");
+    toTime->setFixedHeight(50);
+
+    timeRangeLayout->addWidget(fromLabel);
+    timeRangeLayout->addWidget(fromTime);
+    timeRangeLayout->addWidget(toLabel);
+    timeRangeLayout->addWidget(toTime);
+    timeRangeLayout->setSpacing(0);
+
+    QPushButton * startIcon = new QPushButton();
+    startIcon->setStyleSheet("background-image:url(:/placeholderimage.jpg) no-repeat center center fixed; border: none; margin: 0px;");
+    startIcon->setFixedSize(40, 40);
+
+    QPushButton * deleteIcon = new QPushButton();
+    deleteIcon->setStyleSheet("background-image:url(:/placeholderimage.jpg) no-repeat center center fixed; border: none; margin: 0px;");
+    deleteIcon->setFixedSize(40, 40);
+
+    iconsLayout->addWidget(startIcon);
+    iconsLayout->addWidget(deleteIcon);
+
+    QHBoxLayout * timeAndIconsLayout = new QHBoxLayout();
+    timeAndIconsLayout->addLayout(timeRangeLayout);
+    timeAndIconsLayout->addLayout(iconsLayout);
+    timeAndIconsLayout->setSpacing(20);
+
+    QHBoxLayout * descriptionLayout = new QHBoxLayout();
+    descriptionLayout->setSpacing(0);
+
+    QLabel * descriptionLabel = new QLabel();
+    descriptionLabel->setText(QString("Description: "));
+    descriptionLabel->setStyleSheet("background: rgb(255,240,235);; font-size: 20px; border-top-right-radius: 0px; border-bottom-right-radius: 0px; margin: 0px;");
+    descriptionLabel->setAlignment(Qt::AlignCenter);
+    descriptionLabel->setFixedHeight(50);
+
+    QLineEdit * description = new QLineEdit();
+    description->setStyleSheet("background: rgb(255,240,235);; font-size: 20px; border-top-left-radius: 0px; border-bottom-left-radius: 0px; margin: 0px;");
+    description->setFixedHeight(50);
+
+    descriptionLayout->addWidget(descriptionLabel);
+    descriptionLayout->addWidget(description);
+
+    QVBoxLayout * activitiesContainerLayout = new QVBoxLayout();
+    activitiesContainerLayout->addLayout(timeAndIconsLayout);
+    activitiesContainerLayout->addLayout(descriptionLayout);
+
+    activitiesContainer->setLayout(activitiesContainerLayout);
+
+    dayBoardLayout->insertWidget(numberOfLayoutsInMainLayout-1, activitiesContainer);
+    dayBoardLayout->setAlignment(activitiesContainer, Qt::AlignTop);
+    numberOfLayoutsInMainLayout++;
 }
--- DaySchedule/dayboard.h
+++ DaySchedule/dayboard.h
@@ -2,10 +2,23 @@
 #define DAYBOARD_H
 
 #include <QGroupBox>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QLabel>
 
 class DayBoard : public QGroupBox
 {
     Q_OBJECT
+
+private:
+    QVBoxLayout * dayBoardLayout;
+    size_t numberOfLayoutsInMainLayout;
+
+    void makeDateAndProgressLayout();
+    void makeBottomIconsLayout();
+
+private slots:
+    void addNewActivityLayout();
 
 public:
     explicit DayBoard(QWidget * parent = 0);
